90100e4f37023d49f9338d8adb1d34b31576542b
C0012S <alwaysh1002@gmail.com>
2022-07-22 23:20:50 +0900

    홈 버튼 클릭 시 MediaPlayer 종료 후 페이지 이동하는 다이얼로그 창 띄우기

--- app/src/main/java/com/example/s3urlmediapractice/S3urlvideotestActivity.kt
+++ app/src/main/java/com/example/s3urlmediapractice/S3urlvideotestActivity.kt
@@ -1,23 +1,27 @@
 package com.example.s3urlmediapractice
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.SurfaceHolder
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import kotlinx.android.synthetic.main.activity_s3urlvideotest.*
+import kotlin.properties.Delegates
 
 class S3urlvideotestActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
     private lateinit var surfaceHolder: SurfaceHolder
     private lateinit var mediaPlayer: MediaPlayer
+    private var isPlayed by Delegates.notNull<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,7 @@ class S3urlvideotestActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
         var video_start = findViewById<Button>(R.id.video_start)
         var video_end = findViewById<Button>(R.id.video_end)
+        isPlayed = false
 
         mediaPlayer = MediaPlayer()
         surfaceHolder = video_test_surfaceView.holder
@@ -34,6 +39,7 @@ class S3urlvideotestActivity : AppCompatActivity(), SurfaceHolder.Callback {
             Log.d("Video Start Button : ", "동영상 재생")
 
             mediaPlayer.start()
+            isPlayed = true
         }
 
         video_end.setOnClickListener {
@@ -77,5 +83,37 @@ class S3urlvideotestActivity : AppCompatActivity(), SurfaceHolder.Callback {
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release()
+    }
+
+    override fun onUserLeaveHint() { // 홈 버튼 감지
+        super.onUserLeaveHint()
+
+        Log.d("Home Button : ", "이벤트 감지")
+
+        if (isPlayed) {
+            // 이벤트 작성
+            mediaPlayer.release() // 홈 버튼 누른 후 다시 돌아오면 영상 종료된다. 그 후 다시 '시작' 버튼을 누르면 E/AndroidRuntime: FATAL EXCEPTION: main  Process: com.example.s3urlmediapractice, PID: 9545  java.lang.IllegalStateException: java.lang.IllegalStateException 발생
+
+//            var intent = Intent(applicationContext, MainActivity::class.java)
+//        startActivity(intent) // 인텐트로 페이지 이동 시 홈 버튼을 누른 후 이동할 페이지로 앱이 자동으로 다시 뜬다.
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("동영상 종료")
+                .setMessage("동영상 재생이 종료됩니다.")
+                .setPositiveButton("확인",
+                    DialogInterface.OnClickListener { dialog, id ->
+//                    startActivity(intent) // 인텐트로 페이지 이동 시 페이지 이동 기록이 남아서 뒤로 가기 버튼을 누르면 이 페이지로 다시 돌아온다.
+                        finish() // 액티비티 종료  // 페이지 이동 기록 남지 않는다.
+                    })
+                .setCancelable(false) // 뒤로 가기 버튼과 영역 외 클릭 시 Dialog가 사라지지 않도록 한다.
+            // Dialog 띄워 주기
+            builder.show()
+        }
+
+/*
+        // 앱 종료
+		System.exit(0) // 방법 1
+    	android.os.Process.killProcess(android.os.Process.myPid()) // 방법 2
+*/
     }
 }
